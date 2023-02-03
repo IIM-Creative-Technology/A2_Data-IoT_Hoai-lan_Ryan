682576125d0e83b58c5e6f3773736dc93c74d227
BARDAT Ryan <105910237+REIG3N@users.noreply.github.com>
2023-02-03 09:01:16 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const path = require("path");
+let ip = require("ip");
+console.dir (ip.address());
+let i = 0
+let color = [0,0,0]
+
+app.use(bodyParser.urlencoded({extended: true}));
+const port = 4000
+
+app.get("/", (req,res) => {
+    console.log(req.body);
+    res.send({"data":"vous êtes sur la route get"});
+    //res.json({msg : "vous êtes sur la route GET"});
+})
+
+app.post("/post", (req,res) => {
+    console.log(req.body);
+    res.send({"data":"vous êtes sur la route post"});
+    const sensorData= req.body.data;
+    console.log(`la data : ${sensorData}`);
+    res.send('data received')
+    //res.json({msg : "vous êtes sur la route post"});
+})
+server.listen( port, () => {
+    console.log(`The server is actually listened`)
+})
+// app.post('/api/data', (req, res) => {
+//     console.log(req.body);
+//     res.status(200).send('Data received');
+// });
+
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}`);
+// });
---
+++ index.py
@@ -0,0 +1,128 @@
+from machine import Pin,ADC
+import utime
+import ujson
+import network
+import urequests
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+ssid = 'RyanBgCosmique'
+password = '1234567890'
+wlan.connect(ssid, password)
+url = "http://192.168.202.147:4000/"
+print(wlan.isconnected())
+
+"""while not wlan.isconnected():
+    print("Waiting for co")
+    utime.sleep(1)
+    pass
+"""
+yAxis = ADC(Pin(27))
+joystick= Pin(16,Pin.IN, Pin.PULL_UP)
+
+# data = {"value1": 42, "value2": "hello world"}
+# headers = {'Content-Type': 'application/json'}
+# response = urequests.post(url, json=data, headers=headers)
+# if response.status_code == 200:
+#     print("Data sent successfully")
+# else:
+#     print("Failed to send data")
+
+while True:
+    yValue = yAxis.read_u16()
+    joyStickValue= joystick.value()
+    yStatus = "middle"
+    joystickStatus = "Not pressed"
+    if yValue>=5000 and yValue<=30000:
+        yStatus = "middle"
+    elif yValue <= 80000 and yValue >= 30000 :
+        yStatus = "up"
+    elif yValue >= 600 and yValue <= 5000:
+        yStatus = "down"
+    if joyStickValue == 0:
+        joystickStatus = "pressed"
+    print("X: " + yStatus + " -- Joystic " + joystickStatus)
+    print(str(yValue) +" -- " + str(joyStickValue))
+    # data = {"data":yStatus}
+    # response = urequests.post(url, json=data)
+    utime.sleep(0.1)
+
+"""
+    if yValue>=30000 & yValue<=36000:
+        yStatus = "middle"
+    elif yValue <= 60000 & yValue >= 36000 :
+        yStatus = "up"
+    elif yValue >= 600:
+        yStatus = "down"
+    if joyStickValue == 0:
+        joystickStatus = "pressed"
+"""
+
+"""
+while(True):
+    try:
+        print("GET")
+        r = urequests.get(url)
+        print (r.json())
+        r.close()
+        utime.sleep(1)
+    except Exception as e:
+        print(e)
+        """
+
+
+
+
+"""
+adc = ADC(Pin(26, mode=Pin.IN))
+#pin +-
+while True:
+    val = adc.read_u16()
+    val = val * (3.3 / 65535)
+    print(round(val,2),"V")
+    time.sleep_ms(100)
+"""
+
+"""exo3
+pwm_led = PWM(Pin(15, mode= Pin.OUT))
+pwm_led.freq(1_000)
+while True:
+    for i in range(65000):    
+        pwm_led.duty_u16(i)
+        utime.sleep(0.00001)
+    for i in range(65000):    
+        pwm_led.duty_u16(65000-i)
+        utime.sleep(0.00001)
+
+"""
+
+#Exo2
+#pin_button = Pin(18, mode=Pin.IN, pull = Pin.PULL_UP)
+
+"""Exo1
+pinNumber2 = 13
+led2 = Pin(pinNumber2, mode=Pin.OUT)
+pinNumber1 = 17
+led1 = Pin(pinNumber1, mode=Pin.OUT)
+pinNumber3 = 15
+led3 = Pin(pinNumber3, mode=Pin.OUT)
+"""
+
+#while True:
+
+    
+"""    Exo2
+    print(pin_button.value())
+    utime.sleep(0.5)
+"""
+"""Exo1
+    led1.toggle()
+    utime.sleep(1)
+    led2.toggle()
+    utime.sleep(1)
+    led3.toggle()
+    utime.sleep(1)
+
+"""
+
+
